d9378684cc3997c89e7bc1a9ccb542eb4843c4eb
Georg Meinhardt <habanzu@gmail.com>
2024-05-02 11:38:40 +0200

    Move system instructions to Rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use reqwest::{Error, StatusCode};
+use reqwest::StatusCode;
 use dotenv::dotenv;
 use std::env;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
@@ -131,17 +131,7 @@ async fn return_open_ai_response(request: &OpenAIRequest) -> HttpResponse {
     }
 }
 
-async fn completions(mut body: web::Json<OpenAIRequest>) -> impl Responder {
-    
-    let last_message = match body.messages.last() {
-        Some(message) => {Ok(message.content.clone())},
-        None => {Err("Received message was empty.")},
-    }.expect("Received message was empty.");
-
-    let query = &last_message;
-
-    let google_resp = search_google(query).await;
-    
+fn message_from_google_search(google_resp: GoogleSearchResults) -> Message {
     let mut message = String::from("Google Search results added to question: \n");
 
     for item in google_resp.items {
@@ -153,7 +143,30 @@ async fn completions(mut body: web::Json<OpenAIRequest>) -> impl Responder {
         message.push_str("\n");
     }
 
-    body.messages.push(Message{role : String::from("system"), content: message});
+    Message{role : String::from("system"), content: message}
+}
+
+async fn completions(mut body: web::Json<OpenAIRequest>) -> impl Responder {
+
+    // Create instructions and prepend to request.
+    let instructions = "You are a search engine and personal assistant. A list of the titles and and snippets of the first 10 google results will be provided additionally to the question. Answer the question to the best of your possibility given the limited information. Include links to sources if possible not counting against the word limit.";
+    let instructions  = Message{
+        role: String::from("system"),
+        content: String::from(instructions)
+    };
+
+    body.messages.insert(0, instructions);
+
+    let last_message = match body.messages.last() {
+        Some(message) => {Ok(message.content.clone())},
+        None => {Err("Received message was empty.")},
+    }.expect("Received message was empty.");
+
+    let query = &last_message;
+
+    let google_resp = search_google(query).await;
+
+    body.messages.push(message_from_google_search(google_resp));
  
     return_open_ai_response(&*body).await
 }
